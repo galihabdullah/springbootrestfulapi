c75f021310e380c2d33a1c957ba850f3aca80faa
Ikhsan Fauji <ikhsan3f@gmail.com>
2018-11-16 23:56:17 +0700

    Email Service Solve

--- src/main/java/com/sporthubid/services/EmailService.java
+++ src/main/java/com/sporthubid/services/EmailService.java
@@ -33,56 +33,6 @@ public class EmailService {
         this.javaMailSender = javaMailSender;
     }
 
-    /**
-     * This function is used to send mail without attachment.
-     * @param user
-     * @throws MailException
-     */
-
-    public void sendEmail(Email user) throws MailException {
-
-        /*
-         * This JavaMailSender Interface is used to send Mail in Spring Boot. This
-         * JavaMailSender extends the MailSender Interface which contains send()
-         * function. SimpleMailMessage Object is required because send() function uses
-         * object of SimpleMailMessage as a Parameter
-         */
-
-        SimpleMailMessage mail = new SimpleMailMessage();
-        mail.setTo(user.getEmailAddress());
-        mail.setFrom("ikhsan15fauji@gmail.com");
-        mail.setSubject("Testing Mail API");
-        mail.setText("Hurray ! You have done that dude...");
-
-        /*
-         * This send() contains an Object of SimpleMailMessage as an Parameter
-         */
-        javaMailSender.send(mail);
-    }
-
-    /**
-     * This fucntion is used to send mail that contains a attachment.
-     *
-     * @param user
-     * @throws MailException
-     * @throws MessagingException
-     */
-    public void sendEmailWithAttachment(Email user) throws MailException, MessagingException, javax.mail.MessagingException {
-
-        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-
-        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-
-        helper.setTo(user.getEmailAddress());
-        helper.setSubject("Testing Mail API with Attachment");
-        helper.setText("Please find the attached document below.");
-
-        FileSystemResource file = new FileSystemResource("C:\\Users\\uswa15\\Downloads\\Documents\\FlowDiagramComunity.pdf");
-        helper.addAttachment(file.getFilename(), file);
-
-        javaMailSender.send(mimeMessage);
-    }
-
     @Autowired
     private MailContentBuilder mailContentBuilder;
 
