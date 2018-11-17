b4fe4f21011c549a68772d721308a3f4f7ddcd37
Ikhsan Fauji <ikhsan3f@gmail.com>
2018-11-17 07:08:51 +0700

    deploy backend v1

--- src/main/java/com/sporthubid/SporthubidApplication.java
+++ src/main/java/com/sporthubid/SporthubidApplication.java
@@ -3,10 +3,7 @@ package com.sporthubid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-//import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -15,10 +12,5 @@ public class SporthubidApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(SporthubidApplication.class, args);
 	}
-
-//	@Bean
-//	public BCryptPasswordEncoder bcriptEncoder(){
-//		return 	new BCryptPasswordEncoder();
-//	}
 
 }
