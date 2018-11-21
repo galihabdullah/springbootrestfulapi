fbba2e03140eee4f31b0dedebdba343155ae7751
galih abdullah <galihabdullah471@gmail.com>
2018-11-21 12:37:17 +0700

    make it better

---
+++ src/main/java/com/focusclass13/binarfocus/BinarfocusApplication.java
@@ -0,0 +1,12 @@
+package com.focusclass13.binarfocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinarfocusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinarfocusApplication.class, args);
+    }
+}
