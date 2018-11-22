21dc4097056dce7b90d895b1f494bd62e397e400
Ikhsan Fauji <ikhsan3f@gmail.com>
2018-11-22 12:49:46 +0700

    cors

--- src/main/java/com/sporthubid/controllers/sort/LokasiController.java
+++ src/main/java/com/sporthubid/controllers/sort/LokasiController.java
@@ -3,6 +3,7 @@ package com.sporthubid.controllers.sort;
 import com.sporthubid.models.sort.LokasiModel;
 import com.sporthubid.repository.sort.LokasiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/lokasi")
 public class LokasiController {
     @Autowired
