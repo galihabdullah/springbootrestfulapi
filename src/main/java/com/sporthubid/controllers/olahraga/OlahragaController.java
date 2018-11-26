22bfed3c690313119dddf754e752dedb3db4235c
Ikhsan Fauji <ikhsan3f@gmail.com>
2018-11-26 11:40:42 +0700

    add swagger api documentation

--- src/main/java/com/sporthubid/controllers/olahraga/OlahragaController.java
+++ src/main/java/com/sporthubid/controllers/olahraga/OlahragaController.java
@@ -7,6 +7,7 @@ import com.sporthubid.repository.sort.JenisOlahrgaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -20,6 +21,9 @@ public class OlahragaController {
     @Autowired
     JenisOlahrgaRepository jenisOlahrgaRepository;
 
+    @Autowired
+    EntityManager entityManager;
+
     @GetMapping(path = "/sports")
     public List<JenisOlahragaModel> getOlahraga(){
         return jenisOlahrgaRepository.findAll();
@@ -28,7 +32,7 @@ public class OlahragaController {
     @PutMapping(path = "sports-interest/{id_user}")
     public int setMinat(@Valid @RequestBody UserEdit user, @PathVariable(value = "id_user") Long id_user){
         if (userEditRepository.existsById(id_user)){
-            userEditRepository.setMinatOlahraga(user.getMinat_or(), id_user);
+            entityManager.createNativeQuery("UPDATE tb_user SET minat_or="+user.getMinat_or()+" WHERE id_user="+id_user+"").executeUpdate();
             return 1;
         } else {
             return 0;
--- src/main/java/com/sporthubid/repository/UserEditRepository.java
+++ src/main/java/com/sporthubid/repository/UserEditRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
 public interface UserEditRepository extends JpaRepository<UserEdit, Long> {
 
